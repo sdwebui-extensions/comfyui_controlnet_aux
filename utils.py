03116dfa56bc37ec7ddd414a4779b207d353deb4
宣源 <xuanyuan.lb@alibaba-inc.com>
2024-07-22 09:20:51 +0800

    fix utils

--- utils.py
+++ utils.py
@@ -59,6 +59,7 @@ else:
         annotator_ckpts_path = str(Path(here, "./ckpts"))
     USE_SYMLINKS = False
     ORT_PROVIDERS = ["CUDAExecutionProvider", "DirectMLExecutionProvider", "OpenVINOExecutionProvider", "ROCMExecutionProvider", "CPUExecutionProvider", "CoreMLExecutionProvider"]
+    TEMP_DIR = os.path.join(args.data_dir, 'models/ckpts')
 
 os.environ['AUX_ANNOTATOR_CKPTS_PATH'] = os.getenv('AUX_ANNOTATOR_CKPTS_PATH', annotator_ckpts_path)
 os.environ['AUX_TEMP_DIR'] = os.getenv('AUX_TEMP_DIR', str(TEMP_DIR))
